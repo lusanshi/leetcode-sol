b3f78342992607f0289e833536d6dbc7286569ea
lusansh <lulei_95@163.com>
2019-10-12 11:23:24 +0800

    215

--- 215.数组中的第k个最大元素.c
+++ 215.数组中的第k个最大元素.c
@@ -6,37 +6,60 @@
 
 // @lc code=start
 
-
-int findKthLargest(int* nums, int numsSize, int k){
-    int key;
-    while(numsSize > 0){
+int findKthLargest(int *nums, int numsSize, int k)
+{
+    /*
+    int *bubble;
+    int tmp, i, max;
+    while(k > 0){
+        k--;
+        bubble = nums;
+        for(i = 1; i < numsSize; i++){
+            bubble++;
+            if(*bubble < *(bubble-1)){
+                tmp = *(bubble - 1);
+                *(bubble - 1) = *bubble;
+                *bubble = tmp;
+            }
+        }
+        max = *bubble;
         numsSize--;
-
     }
-    return *nums + 3;
+    return max;
+    */
+    quickSelectSort(nums, numsSize, k);
+    return *(nums + k - 1);
 }
 
-
-void quicksort(int *nums, int numsSize){
-    int key, i, count;
-    int *slide;
-    i = 0;
-    count = 0;
-    key = *nums + numsSize - 1;
-    while(i < numsSize-1){
-        if(*nums+i >= key){
-            swap(*nums, *nums-count);
-        }
-        else{
+void quickSelectSort(int *p, int numsSize, int k)
+{
+    int count = 0, tmp;
+    int n = numsSize;
+    int key = *(p + numsSize - 1);
+    int *nums;
+    
+    nums = p;
+    
+    while (numsSize > 0)
+    {
+        if (*p < key)
             count++;
+        else
+        {
+            tmp = *p;
+            *p = *(p - count);
+            *(p - count) = tmp;
         }
-        i++;
+        p++;
+        numsSize--;
     }
-    quicksort(*nums, numsSize - count);
-    quicksort(*nums + numsSize - count, count);
-    return;
-}
 
+    if (k == n - count || n <= 2)
+        return;
+    else if (k < n - count)
+        quickSelectSort(nums, n - count - 1, k);
+    else
+        quickSelectSort(nums + n - count, count, k - n + count);
+}
 
 // @lc code=end
-
--- demo.c
+++ demo.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
 
-int main(){
-    int array[6] = {1, 2, 3, 4, 5, 6};
-    int *index;
-    index = array;
-    printf("array[6]=%d", *index+5);
+int findKthLargest(int*, int, int);
+void quickSelectSort(int*, int, int);
+
+int main()
+{
+    int nums[6] = {1, 9, 3, 8, 5, 7};
+    int k = 4;
+    int ans;
+    ans = findKthLargest(nums, 6, 4);
+    printf("The Kth number of the array is: %d", ans);
     return 0;
+}
+
+int findKthLargest(int *nums, int numsSize, int k)
+{
+    quickSelectSort(nums, numsSize, k);
+    return *(nums + k - 1);
+}
+
+void quickSelectSort(int *p, int numsSize, int k)
+{
+    int count = 0, tmp;
+    int n = numsSize;
+    int key = *(p + numsSize - 1);
+    int *nums;
+    nums = p;
+    while (numsSize > 0)
+    {
+        if (*p <= key)
+            count++;
+        else
+        {
+            tmp = *p;
+            *p = *(p - count);
+            *(p - count) = tmp;
+        }
+        p++;
+        numsSize--;
+    }
+    if (k <= n - count)
+        quickSelectSort(nums, n - count, k);
+    else
+        quickSelectSort(nums + n - count, count, k - n + count);
 }
